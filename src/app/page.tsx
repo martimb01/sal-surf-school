646f2cf9113d3a34a2941eb31bab212f711d45aa
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-06-26 23:15:35 +0100

    Fixed mini scroll bar bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Welcome to sals surfing School</h1>
       <main className={styles.main}>
 
       </main>
