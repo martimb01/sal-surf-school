a4cb4bd95f868304f1dd165307be25ae0549c7b7
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-06-28 00:36:24 +0100

    Further work on first section of the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Carousel from "../components/Carousel";
 import {VT323} from 'next/font/google'
 import Button from "@/components/ui/Button";
 import { GiSurferVan } from "react-icons/gi";
+import { MdHealthAndSafety } from "react-icons/md";
+import { GiWaveSurfer } from "react-icons/gi";
 import PresentationCard from "@/components/ui/PresentationCard";
 
 //VT323 Font properties
@@ -26,8 +28,24 @@ export default function Home() {
             <Carousel />
           </div>
         </div>
-        <div className={styles.firstSection}>
-          <PresentationCard icon={<GiSurferVan size={150}/>} />
+        <div className={styles.firstSection}>          
+          <PresentationCard 
+            icon={<MdHealthAndSafety size={150}/>}
+            title={'Safety first'}
+            info={'Our instructors are profesionaly trained, making sure its a safe and fun experience!'}
+          />
+          <hr/>
+          <PresentationCard 
+            icon={<GiSurferVan size={150}/>}
+            title={'Transport'}
+            info={'Well grab u anywhere in Lisbon! Just let us know where we should pick you up!'}
+          />
+          <hr />
+          <PresentationCard 
+            icon={<GiWaveSurfer size={150}/>}
+            title={'Fun'}
+            info={'Above all, well make sure we make your lesson worthwhile! No stress, just fun!'}
+          />          
         </div>
       </main>
     </div>
--- src/components/ui/PresentationCard.tsx
+++ src/components/ui/PresentationCard.tsx
@@ -1,19 +1,21 @@
+type Props = {
+  icon: React.ReactNode;
+  title: string;
+  info: string;
+};
 
 import styles from './PresentationCard.module.css'
-export default function PresentationCard(
-    {icon}: {icon: React.ReactNode}
-) {
+export default function PresentationCard({icon, title, info}: Props) {
     return(
         <div className={styles.card}>
             <div className={styles.icon}>
                 {icon}
             </div>
             <div className={styles.cardInfo}>
-                <h1 className={styles.infoTitle}>Transport</h1>
+                <h1 className={styles.infoTitle}>{title}</h1>
                 <h2 className={styles.infoText}>
                     <hr />
-                    Well grab u anywhere in Lisbon! Lets kno
-                    
+                    {info}
                 </h2>
             </div>
         </div>
