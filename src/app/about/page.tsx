83e761ebdef938ad2079309a0dc46833515ef1c8
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-06-29 20:26:29 +0100

    Added background video to about us section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,25 @@
 import styles from './page.module.css'
+import {VT323} from 'next/font/google'
+
+//VT323 Font properties
+const vt323 = VT323({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export default function page() {
   return (
-    <main className={styles.main}>
-        Helloooss
+    <main className={`${styles.main} ${vt323.className}`}>
+      <h1>About us</h1>
+      <h2>Origin</h2>
+        <video
+          className={styles.video}
+          autoPlay
+          muted
+          loop
+          src="/barrel.mp4"
+        >
+        </video>
     </main>
   )
 }
